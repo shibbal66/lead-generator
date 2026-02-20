89771da8783aa619a40ecb4a6de04bafb0a59f41
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-02-21 00:11:19 +0500

    frontend

--- config/env.ts
+++ config/env.ts
@@ -1,4 +1,3 @@
-export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || "https://lead-generator-pro.vercel.app";
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://lead-generator-pro-otga.onrender.com/api";
-export const FIREBASE_VAPID_KEY = import.meta.env.VITE_FIREBASE_VAPID_KEY || "BITIsZOW5crpbBWm1JsS_mzys94csxkdSYhDTX9xsoP55IRW_vIMPO1Ga7_U8c5uAGfbgES-wGxFFT-l6aOPswA"
-  ;
+export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const FIREBASE_VAPID_KEY = import.meta.env.VITE_FIREBASE_VAPID_KEY;
